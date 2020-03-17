f27331d05165eaa3a74c3e6fff0f4cca47cccfc5
12Helena1228 <helena.binkova.uni@gmail.com>
2020-03-17 15:38:25 +0100

    Partie 2.1

--- partie2/src/Lab/Lab.hpp
+++ partie2/src/Lab/Lab.hpp
@@ -1,11 +1,19 @@
 #ifndef LAB_HPP
 #define LAB_HPP
 
+#include "../Application.hpp"
+#include "PetriDish.hpp"
 
 class Lab
 {
 public:
     Lab();
+
+
+
+private:
+    PetriDish petridish_;
+
 };
 
 #endif // LAB_HPP
--- partie2/src/Lab/PetriDish.cpp
+++ partie2/src/Lab/PetriDish.cpp
@@ -1,6 +1,84 @@
 #include "PetriDish.hpp"
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include "Utility/Vec2d.hpp"
+#include "Utility/Utility.hpp"
 
-PetriDish::PetriDish()
+PetriDish::PetriDish(const Vec2d& poscenter, const double radius)
+    : CircularBody(poscenter, radius)
+{}
+
+PetriDish::~PetriDish()
 {
-
+    reset();
 }
+
+
+
+bool PetriDish::addBacterium(Bacterium* bacterium)
+{
+    bacteria_.push_back(bacterium);
+    //Conditions à ajouter
+}
+
+bool PetriDish::addNutriment(Nutriment* nutriment)
+{
+    nutriments_.push_back(nutriment);
+    //Conditions à ajouter;
+}
+
+void PetriDish::update(sf::Time dt)
+{
+    //A remplir : fonciton qui fait evoluer toutes les bacteries/nutriments)
+    //à chaque pas de temps
+}
+
+void PetriDish::drawOn(sf::RenderTarget& targetWindow) const
+{
+    auto border = buildAnnulus(poscenter_, radius_, sf::Color::Black, 5);
+    target.draw(border);
+}
+
+void PetriDish::reset()
+{
+    //Désalloue la mémoire occupée par les bactéries, puis vide l'ensemble des bactéries.
+    for (auto& bacterium : bacteria_)
+    {
+        delete bacterium;
+    }
+    bacteria_.clear();
+
+    //Désalloue la mémoire occupée par les nutriments, puis vide l'ensemble des nutriments.
+    for (auto& nutriment : nutriments_)
+    {
+        delete nutriment;
+    }
+    nutriments_.clear();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
