ab4bbd5ca7279bba0672df0f71868b842c1414c6
Harpine <luminou@aline.name>
2020-03-17 15:56:09 +0100

    ca marche pas euh!

--- partie2/src/Lab/Lab.hpp
+++ partie2/src/Lab/Lab.hpp
@@ -1,12 +1,17 @@
 #ifndef LAB_HPP
 #define LAB_HPP
 #include "CircularBody.hpp"
+#include "../Application.hpp"
+#include "PetriDish.hpp"
 
 class Lab
 {
 public:
     Lab();
     bool contains(const CircularBody& circularbody);
+
+private:
+    PetriDish petridish_;
 };
 
 #endif // LAB_HPP
--- partie2/src/Lab/PetriDish.hpp
+++ partie2/src/Lab/PetriDish.hpp
@@ -1,11 +1,49 @@
 #ifndef PETRIDISH_HPP
 #define PETRIDISH_HPP
 
+#include <vector>
+#include <SFML/Graphics.hpp>
+#include "CircularBody.hpp"
+#include "Bacterium.hpp"
+#include "Nutriment.hpp"
+#include "Utility/Vec2d.hpp"
 
-class PetriDish
+class PetriDish : public CircularBody
 {
 public:
-    PetriDish();
+
+    //Constructeurs et destructeurs:
+    //Constructeur :
+    PetriDish(const Vec2d& poscenter, const double radius);
+    //Interdiction de copie d'une assiette de pétri
+    PetriDish(const PetriDish& p) = delete;
+    //Destruction de l'assiette de pétri ainsi que des nutriments et bactéries qui l'habitent
+    ~PetriDish();
+
+    //Surcharges d'operateurs :
+    //Interdiction d'affectation entre assiettes de pétri
+    PetriDish& operator=(const PetriDish& p) = delete;
+
+
+    //Methodes :
+    //Ajoute une bactérie à l'ensemble de bactéries de l'assiette
+    bool addBacterium(Bacterium* bacterium);
+    //Ajoute un nutriment à l'ensemble des nutriments de l'assiette
+    bool addNutriment(Nutriment* nutriment);
+    //Fait évoluer l'assiette de pétri à chaque intervalle de temps
+    void update(sf::Time dt);
+    //Représentation graphique de l'assiette de pétri
+    void drawOn(sf::RenderTarget& targetWindow) const;
+    //Supprime nutriments et bactéries de l'assiette
+    void reset();
+
+
+
+
+private:
+    std::vector<Bacterium*> bacteria_;
+    std::vector<Nutriment*> nutriments_;
+
 };
 
 #endif // PETRIDISH_HPP
