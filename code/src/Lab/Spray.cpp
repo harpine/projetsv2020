a4947754ed5184cf11d8c48b6fcaadf45ca0aeee
12Helena1228 <helena.binkova.uni@gmail.com>
2020-05-19 10:59:53 +0200

    POur le merge

--- code/src/Lab/Spray.cpp
+++ code/src/Lab/Spray.cpp
@@ -1,6 +1,33 @@
 #include "Spray.hpp"
+#include <Application.hpp>
+#include <Utility/Utility.hpp>
 
 Spray::Spray()
 {
 
 }
+
+
+
+
+
+j::Value& Spray::getConfig() const
+{
+    return getAppConfig()["Spray"];
+}
+
+void Spray::drawOn(sf::RenderTarget& target) const
+{
+    int taille_graphique(6);
+    auto const& texture = getAppTexture(getConfig()["texture"].toString());
+    auto spraySprite = buildSprite(getPosition(), taille_graphique, texture);
+    // adapte la taille du Sprite au rayon du nutriment:
+    spraySprite.setScale(2 * getRadius() / texture.getSize().x, 2 * getRadius() / texture.getSize().y);
+    target.draw(spraySprite);
+}
+
+bool Spray::hasFaded() const
+{
+    return clock_.getElapsedTime().asSeconds() > getConfig()["fading time"].toDouble();
+}
+
--- code/src/Lab/Spray.hpp
+++ code/src/Lab/Spray.hpp
@@ -1,16 +1,27 @@
 #ifndef SPRAY_HPP
 #define SPRAY_HPP
 #include "CircularBody.hpp"
+#include "Interface/Drawable.hpp"
 #include <Utility/Vec2d.hpp>
 
 
-class Spray : public CircularBody
+class Spray : public CircularBody, public Drawable
 {
 public:
     //Constructeurs et destructeurs:
     Spray(const Vec2d& poscenter);
     ~Spray();
     Spray(const Spray& other);
+
+    //Getters et setters:
+    j::Value& getConfig() const;
+
+    //Autres méthodes:
+    void drawOn(sf::RenderTarget& target) const override;
+    bool hasFaded() const;
+
+private :
+    sf::Clock clock_;
 };
 
 #endif // SPRAY_HPP
