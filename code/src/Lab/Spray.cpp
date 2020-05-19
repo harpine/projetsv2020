14ab0fbbb059316be188a7f2cae41d70d954bd85
Harpine <luminou@aline.name>
2020-05-19 16:54:28 +0200

    sound

--- code/src/Lab/Spray.cpp
+++ code/src/Lab/Spray.cpp
@@ -1,10 +1,25 @@
 #include "Spray.hpp"
 #include <Application.hpp>
 #include <Utility/Utility.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Audio/Sound.hpp>
 
 Spray::Spray(const Vec2d& poscenter)
     :CircularBody(poscenter, getConfig()["size"].toDouble())
-{}
+{
+//    sf::Music music;
+//    music.openFromFile(getApp().getResPath() + getConfig()["audio"].toString());
+//    music.setVolume(100);
+//    music.play();
+    sf::SoundBuffer* sprayAudio = new sf::SoundBuffer();
+    if (sprayAudio->loadFromFile(getApp().getResPath() + getConfig()["audio"].toString())) //évite de faire crasher le programme s'il y a un problème avec le fichier audio
+    {
+        std::cerr << "test";
+        sf::Sound spray;
+        spray.setBuffer(*sprayAudio);
+        spray.play();
+    }
+}
 
 j::Value& Spray::getConfig() const
 {
